b3c7c44c5058de72da97c04d394933a67d4951a2
Michael Overmeyer <m.overmeyer@yahoo.ca>
2018-10-05 10:31:35 -0400

    Added tooling to automatically publish to PyPI on tag creation.

--- setup.py
+++ setup.py
@@ -1,9 +1,30 @@
 #!/usr/bin/env python
+import os
+import sys
 from setuptools import setup
+from setuptools.command.install import install
+
+VERSION = '0.0.1'
+
+
+class VerifyVersionCommand(install):
+    # https://circleci.com/blog/continuously-deploying-python-packages-to-pypi-with-circleci/
+    """Custom command to verify that the git tag matches our version"""
+    description = 'verify that the git tag matches our version'
+
+    def run(self):
+        tag = os.getenv('CIRCLE_TAG')
+
+        if tag != VERSION:
+            info = "Git tag: {0} does not match the version of this app: {1}".format(
+                tag, VERSION
+            )
+            sys.exit(info)
+
 
 setup(
     name='publish_test',
-    version='0.0.1',
+    version=VERSION,
     description='A test of publishing to PyPI using CircleCI on merge into master',
     author='Michael Overmeyer',
     author_email='test@example.com',
@@ -13,4 +34,7 @@ setup(
 
     classifiers=[],
     keywords='',
+    cmdclass={
+        'verify': VerifyVersionCommand,
+    }
 )
